7fc36135570b4061ca221e5982dae1759baeb254
Ramanan Sambandam <ramanans@ilink-systems.com>
2017-11-15 16:22:56 +0530

    Initial project commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Weather.WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbCities = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblHigh = new System.Windows.Forms.Label();
+            this.lblLow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbCities
+            // 
+            this.cbCities.FormattingEnabled = true;
+            this.cbCities.Items.AddRange(new object[] {
+            "Chennai",
+            "Mumbai",
+            "Delhi"});
+            this.cbCities.Location = new System.Drawing.Point(21, 23);
+            this.cbCities.Name = "cbCities";
+            this.cbCities.Size = new System.Drawing.Size(241, 21);
+            this.cbCities.TabIndex = 0;
+            this.cbCities.SelectedIndexChanged += new System.EventHandler(this.cbCities_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Goudy Old Style", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "High";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Goudy Old Style", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Low";
+            // 
+            // lblHigh
+            // 
+            this.lblHigh.AutoSize = true;
+            this.lblHigh.Font = new System.Drawing.Font("Goudy Old Style", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHigh.Location = new System.Drawing.Point(143, 67);
+            this.lblHigh.Name = "lblHigh";
+            this.lblHigh.Size = new System.Drawing.Size(49, 23);
+            this.lblHigh.TabIndex = 3;
+            this.lblHigh.Text = ": 0°C";
+            // 
+            // lblLow
+            // 
+            this.lblLow.AutoSize = true;
+            this.lblLow.Font = new System.Drawing.Font("Goudy Old Style", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLow.Location = new System.Drawing.Point(143, 111);
+            this.lblLow.Name = "lblLow";
+            this.lblLow.Size = new System.Drawing.Size(49, 23);
+            this.lblLow.TabIndex = 4;
+            this.lblLow.Text = ": 0°C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 169);
+            this.Controls.Add(this.lblLow);
+            this.Controls.Add(this.lblHigh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCities);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Yahoo Weather";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbCities;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHigh;
+        private System.Windows.Forms.Label lblLow;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weather.WinForm
+{
+    public partial class Form1 : Form
+    {
+        public List<Weather> forecastList;
+        public string[] Cities = { "Chennai", "Mumbai", "Delhi" };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cbCities.SelectedIndex = 0;
+        }
+
+        private void cbCities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            forecastList = WeatherServiceManager.Instance.GetForecast(Cities[cbCities.SelectedIndex]);
+            updateTemperature();
+        }
+
+        void updateTemperature()
+        {
+            lblHigh.Text = ": " + (forecastList.Count > 0 ? forecastList[0].High : "0") + "°C";
+            lblLow.Text = ": " + (forecastList.Count > 0 ? forecastList[0].Low : "0") + "°C";
+        }
+    }
+}
